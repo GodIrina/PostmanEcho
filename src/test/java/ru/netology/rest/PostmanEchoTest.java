c133bb4cf9d0f6b9043eac0c5694ecd4e2ecdb78
irina godonoage <godonoage@bk.ru>
2022-07-28 14:26:02 +0300

    commit

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -41,23 +41,6 @@ public class PostmanEchoTest {
     }
 
     @Test
-    public void shouldSendRequestBody3() {
-        // Given - When - Then
-// Предусловия
-        given()
-                .baseUri("https://postman-echo.com")
-                .contentType("text/plain; charset=UTF-8")
-                .body("Нетология") // отправляемые данные (заголовки и query можно выставлять аналогично)
-// Выполняемые действия
-                .when()
-                .post("/post")
-                //Проверки
-                .then()
-                .statusCode(200)
-                .body("data", equalTo("Нетология"))
-        ;
-    }
-    @Test
     void shouldSendRequestHeader() {
         // Given - When - Then
         // Предусловия
